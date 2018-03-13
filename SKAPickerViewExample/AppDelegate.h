a2200c2876952898f0e480f6bc7b1b1677541f98
Ira Shah <aloksingh@Aloks-MacBook-Pro.local>
2018-03-13 11:23:52 +0530

    Initial Commit

---
+++ SKAPickerViewExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SKAPickerViewExample
+//
+//  Created by Alok Singh on 13/03/18.
+//  Copyright © 2018 Alok Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SKAPickerViewExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SKAPickerViewExample
+//
+//  Created by Alok Singh on 13/03/18.
+//  Copyright © 2018 Alok Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
